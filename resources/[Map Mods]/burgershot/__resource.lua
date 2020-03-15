b65ab334252a82983b8b08073ee3f229f323eb78
AceDax <38195478+AceDax@users.noreply.github.com>
2020-03-15 17:07:53 -0400

    Add files via upload

---
+++ resources/[Map Mods]/burgershot/__resource.lua
@@ -0,0 +1,9 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+this_is_a_map 'yes'
+
+files {
+	'bs_timecycmod.xml'
+}
+
+data_file 'TIMECYCLEMOD_FILE' 'bs_timecycmod.xml'
+data_file 'SCALEFORM_DLC_FILE' 'stream/int1756029552.gfx'
