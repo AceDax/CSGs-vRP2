dff4c5355cd2ed57cc2e28caca201dc68f92199c
Jonathan Geer <acedaxx@gmail.com>
2020-07-20 12:09:55 -0400

    Added dashcam script

---
+++ resources/[Police Addons]/dashcam/config.lua
@@ -0,0 +1,10 @@
+DashcamConfig = {}
+
+DashcamConfig.useMPH = true -- False will turn it to KMH
+
+DashcamConfig.RestrictVehicles = true
+DashcamConfig.RestrictionType = "class" -- custom / class
+
+DashcamConfig.AllowedVehicles = {
+    "police2",
+}
