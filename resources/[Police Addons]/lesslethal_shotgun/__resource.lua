cdacbcce0e7a7d15b8f7a6a40f24e1bc4388a64f
Jonathan Geer <acedaxx@gmail.com>
2020-07-20 12:17:35 -0400

    added bean bag shotgun weapon

---
+++ resources/[Police Addons]/lesslethal_shotgun/__resource.lua
@@ -0,0 +1,17 @@
+resource_manifest_version '679-996150c95a1d251a5c0c7841ab2f0276878334f7'
+
+files {
+  'weaponcomponents.meta',
+  'loadouts.meta',
+  'pedpersonality.meta',
+  'weaponarchetypes.meta',
+  'weaponanimations.meta',
+  'weapons.meta'
+}
+
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'weaponcomponents.meta'
+data_file 'WEAPON_METADATA_FILE' 'weaponarchetypes.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'weaponanimations.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'weapons.meta'
+data_file 'LOADOUTS_FILE' 'loadouts.meta'
+data_file 'PED_PERSONALITY_FILE' 'pedpersonality.meta'
