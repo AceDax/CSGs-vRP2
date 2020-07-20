0c5348604cbf244d998b78e28e819bfbedfabfc3
Jonathan Geer <acedaxx@gmail.com>
2020-07-20 11:33:00 -0400

    removed hat mask and glasses script

--- resources/[Server Tools]/hat-mask-glasses/README.md
+++
@@ -1,5 +0,0 @@
-# Hat
-Simple /hat script to toggle ped hat on/off
-
-FiveM post: https://forum.fivem.net/t/hat-removal-script/243240
-Credit to brickyard for the layout
--- resources/[Server Tools]/hat-mask-glasses/__resource.lua
+++
@@ -1,8 +0,0 @@
---[[
-
-Simple script that takes on and off your sunglasses with the /sg command
-Made for Citylife:RP by Brickyard
-http://www.twitch.tv/brickyard_tv
-
-]]--
-client_script "client.lua"
